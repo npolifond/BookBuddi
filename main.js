bb8c55e1bf27c64f6974ab8a0405fcc425abc061
npolifond <80662339+npolifond@users.noreply.github.com>
2023-04-19 04:38:30 -0400

    trying to fix issue with size of old image

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var title;
+  var author;
+  var img;
+  var description;
+  var purchase;
+
   $("#searchform").submit(function() {
     const searchTerm = $("#titleName").val();
     const selection = $('input[name="search"]:checked').val();
@@ -10,6 +16,17 @@ $(document).ready(function() {
         case 'ISBN10':
           if (validIsbn(searchTerm)) {
             $.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`, function(response) {
+              for(i=0;i<response.items.length;i++){
+
+                title=$('<h5 class="tile">'+response.items[i].volumeInfo.title+ '</h5>');
+                author=$('<h5 class="author">'+response.items[i].volumeInfo.authors+ '</h5>');
+                description=$('<p class="desc">'+response.items[i].volumeInfo.description+ '</p>');
+                img=$('<img class="imgNail" id="thumbnail"><br>+<a href='+response.items[i].volumeInfo.imageLinks.thumbnail+ '+ <button id="selectButton" class="select">select</button> </a>');
+                purchase=$('<p class="img">'+response.items[i].saleInfo.buyLink+ '</p>');
+               // imag.attr('src')
+               //https://codepen.io/Kicky/pen/ZxvvqE
+               
+              }
               console.log(response);
             }).fail(function() {
               alert('Error: Could not retrieve book information.');
@@ -24,6 +41,17 @@ $(document).ready(function() {
             alert("ISBN10 has been entered. Please enter correct input.");
           } else {
             $.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`, function(response) {
+              for(i=0;i<response.items.length;i++){
+
+                title=$('<h5 class="tile">'+response.items[i].volumeInfo.title+ '</h5>');
+                author=$('<h5 class="author">'+response.items[i].volumeInfo.authors+ '</h5>');
+                description=$('<p class="desc">'+response.items[i].volumeInfo.description+ '</p>');
+                img=$('<img class="imgNail" id="thumbnail"><br>+<a href='+response.items[i].volumeInfo.imageLinks.thumbnail+ '+ <button id="selectButton" class="select">select</button> </a>');
+                purchase=$('<p class="img">'+response.items[i].saleInfo.buyLink+ '</p>');
+               // imag.attr('src')
+               //https://codepen.io/Kicky/pen/ZxvvqE
+               
+              }
               console.log(response);
             }).fail(function() {
               alert('Error: Could not retrieve book information.');
@@ -36,6 +64,17 @@ $(document).ready(function() {
             alert("ISBN10 has been entered. Please enter correct input.");
           } else {
             $.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`, function(response) {
+              for(i=0;i<response.items.length;i++){
+
+                title=$('<h5 class="tile">'+response.items[i].volumeInfo.title+ '</h5>');
+                author=$('<h5 class="author">'+response.items[i].volumeInfo.authors+ '</h5>');
+                description=$('<p class="desc">'+response.items[i].volumeInfo.description+ '</p>');
+                img=$('<img class="imgNail" id="thumbnail"><br>+<a href='+response.items[i].volumeInfo.imageLinks.thumbnail+ '+ <button id="selectButton" class="select">select</button> </a>');
+                purchase=$('<p class="img">'+response.items[i].saleInfo.buyLink+ '</p>');
+               // imag.attr('src')
+               //https://codepen.io/Kicky/pen/ZxvvqE
+
+              }
               console.log(response);
             }).fail(function() {
               alert('Error: Could not retrieve book information.');
@@ -50,6 +89,14 @@ $(document).ready(function() {
     }
     return false;
   });
+});
+
+/* functions for buttons */
+$(document).ready(function() {
+
+
+
+
 });
 
 function validIsbn(ISBN) {
--- main.py
+++ main.py
@@ -2,21 +2,401 @@ import pymysql
 from app import app
 from db_config import mysql
 from flask import jsonify
-from flask import flash, request
+from flask import flash, request,session, redirect, url_for
 
 
-def search():
-    data= request.get_json()
-    title=data["title"]
-    author=data["author"]
+@app.route('/results', methods=['Get'])
+def getBooks():
+    searchTerm=request.args.get('q')
+    if not searchTerm:
+        message={
+        'status': '400',
+        'message': 'Search term is required.'
+            }
+        resp=jsonify(message)
+        resp.status_code=500
+        return resp
+    
+    url=f'https://www.googleapis.com/books/v1/volumes?q={searchTerm}'
+    resp=request.get(url)
+    results= resp.json()
+    res= jsonify(results)
 
-    link= "https://www.googleapis.com/books/v1/volumes?q="
+    if resp.status_code !=200:
+        message={
+            'message':'Could not retrieve book information.'
+        }
+        resp=jsonify(message)
+        resp.status_code=500
+        return resp
+return res
 
-    if title:
-        url += f"intitle:{title}"
 
-    if author:
-        url+= f"+inauthor:{author}"
-    response= requests.get(url)
-    results= response.json()
-    return jsonify(results)
+#Return users Reviews from review tables when people search for a books. (post the book reviews)
+@app.route('catalog/results/<str:title>', methods=['POST'])
+def reviewsByTitle(title):
+    try:
+        conn=mysql.connect();
+        curr=conn.cur(pymysql.cursors.DictCursors)
+
+        cur.execute("SELECT * FROM review_details WHERE BookTitle = %s;",title)
+        rows = cur.fetchall()
+        if len(rows) > 0:
+            resp = jsonify(rows)
+            resp.status_code = 200
+            return resp
+
+        else:
+            message = {
+                'status': 414,
+                'message': 'The user with the ID specified does NOT exist'
+            }
+            resp = jsonify(message)
+            resp.status_code = 414
+            return resp
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+@app.route('/user/account',methods = ['GET'])
+def reviewsbyUser(id):
+    try:
+        conn=mysql.connect();
+        curr=conn.cur(pymysql.cursors.DictCursors)
+
+        cur.execute("SELECT * FROM review_details WHERE user_id = %s;",id)
+        rows = cur.fetchall()
+        if len(rows) > 0:
+            resp = jsonify(rows)
+            resp.status_code = 200
+            return resp
+
+        else:
+            message = {
+                'status': 414,
+                'message': 'The user with the ID specified does NOT exist'
+            }
+            resp = jsonify(message)
+            resp.status_code = 414
+            return resp
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+    
+
+
+#install flask-Session and mysql-connector-python
+@app.route('/catalog/title',methods=['POST'])
+def SubmitReview(id):
+    try:
+        if 'id' not in session:
+            return redirect(url_for('login page.html'))
+
+        BookTitle=request.form['title']
+        Description=request.form['description']
+        Rating= request.form['rating']
+
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        if title and description and rating:
+
+            sql="INSERT INTO review_details (BookTitle, Description, Rating, user_id, id) VALUES(%s, %s, %s,%s,NULL)"
+            data = (title, description, rating, id, NULL)
+            message={
+                'message':'Review has been submitted',
+                'status':'200'
+            }
+            resp = jsonify(message)
+            resp.status_code = 200
+      
+        else:
+            message = {
+                'status': 510,
+                'message': 'Some of the fields are empty'
+            }
+            resp = jsonify(message)
+            resp.status_code = 510
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+        return resp 
+        
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+
+
+#Delete users reviews
+@app.route('/user/delete/<str:title>/<int:id>')
+def removeReview(BookTitle,id):
+    try:
+        #MySQL connection
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor) #The function is actually cursor(), not cur()
+        
+        cur.execute("SELECT * FROM review_details WHERE user_id = %s;",id)
+        rows = cur.fetchall()
+
+        if len(rows) > 0:
+            cur.execute("DELETE FROM review_details WHERE BookTitle = %s;",BookTitle)
+            conn.commit()
+            message = {
+                'status': 200,
+                'message': 'The review  of '+ BookTitle +' was deleted successfully'
+            }
+            resp = jsonify(message)
+            resp.status_code = 414
+            return resp
+
+        else:
+            message = {
+                'status': 414,
+                'message': 'The book does NOT exist'
+            }
+            resp = jsonify(message)
+            resp.status_code = 414
+            return resp
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+#update User Reviews
+@app.route('/user/update',  methods=['POST'])
+def updateReview(BookTitle,id):
+    try:
+        id = request.form['userId']
+        username = request.form['username']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        #MySQL connection
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        if username and email and phone:
+            
+            sql = "UPDATE review_details SET username = %s,email = %s,phone = %s WHERE id = %s;"
+            data = (username, email, phone, id)
+
+            cur.execute(sql, data)
+            conn.commit()
+
+            message = {
+                'status': 200,
+                'message': 'The user was modified successfully'
+            }
+            resp = jsonify(message)
+            resp.status_code = 200
+        
+        else:
+            message = {
+                'status': 510,
+                'message': 'Some of the fields are empty'
+            }
+            resp = jsonify(message)
+            resp.status_code = 510
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+        return resp
+    
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+    
+
+###################################################################################################################3
+
+#Get records from a specific table in JSON format
+@app.route('/users') #Changed the path to 'users'
+def user():
+    try:
+        #MySQL connection
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor) #The function is actually cursor(), not cur()
+        
+        cur.execute("SELECT * FROM users;")
+        rows = cur.fetchall()
+        print("Records returned: "+str(len(rows)))
+
+        if len(rows) > 0:
+            resp = jsonify(rows)
+            resp.status_code = 200
+            return resp
+
+        else:
+            message = {
+                'status': 404,
+                'message': 'The table is empty'
+            }
+            resp = jsonify(message)
+            resp.status_code = 404
+            return resp
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+@app.route('/users/<int:username>')
+def view_user(id):
+    try:
+        #MySQL connection
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor) #The function is actually cursor(), not cur()
+        
+        cur.execute("SELECT * FROM users WHERE username = %s;",username)
+        rows = cur.fetchall()
+        print("Records returned: "+str(len(rows)))
+
+        if len(rows) > 0:
+            resp = jsonify(rows)
+            resp.status_code = 200
+            return resp
+
+        else:
+            message = {
+                'status': 414,
+                'message': 'The user with the ID specified does NOT exist'
+            }
+            resp = jsonify(message)
+            resp.status_code = 414
+            return resp
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+#add account info into database 
+@app.route('/users/create', methods=['POST'])
+def add_user():
+    try:
+        username = request.form['username']
+        email = request.form['email']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        password = request.form['password']
+
+        #MySQL connection
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        if firstname and lastname and email and username and  password:
+
+            sql = "INSERT INTO users (firstname, lastname, email, username, password) VALUES(%s, %s, %s, %s, %s)"
+            data = (firstname, lastname, email, username, password)
+
+            cur.execute(sql, data)
+            conn.commit()
+
+            message = {
+                'status': 200,
+                'message': 'The user was created successfully'
+            }
+            resp = jsonify(message)
+            resp.status_code = 200
+        
+        else:
+            message = {
+                'status': 510,
+                'message': 'Some of the fields are empty'
+            }
+            resp = jsonify(message)
+            resp.status_code = 510
+
+        cur.close() #The finally block was removed and its content was placed here
+        conn.close()
+
+        return resp
+    
+    except Exception as e: #(If there is an error, it will be returned in a JSON format)
+        message = {
+        'status': 500,
+        'message': 'Error: '+str(e)
+        }
+        resp = jsonify(message)
+        resp.status_code = 500
+        return resp
+
+
+
+#List records in a table using JavaScript+AJAX
+#Custom search (using GET method)
+
+@app.errorhandler(404)
+def not_found(error = None):
+    message = {
+        'status': 404,
+        'message': 'Not found: '+request.url
+    }
+
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+if __name__ == "__main__":
+    app.run()
